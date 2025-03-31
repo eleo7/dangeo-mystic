df9572315737261c39389f4de097b684f44d0cc9
eleo7 <143750200+eleo7@users.noreply.github.com>
2025-03-31 13:22:54 -0300

    Add files via upload

--- dangeo_mystic_site/app.py
+++ dangeo_mystic_site/app.py
@@ -1,17 +1,19 @@
 
 from flask import Flask, render_template
 from auth import auth_bp
+import folium
 import os
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
-# Registrar o blueprint de autenticação
 app.register_blueprint(auth_bp)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    m = folium.Map(location=[0, 0], zoom_start=2, tiles=None)
+    folium.Marker(location=[10, 10], popup="Raça Teste").add_to(m)
+    return render_template("index.html", map_html=m._repr_html_())
 
 if __name__ == "__main__":
     app.run(debug=True)
